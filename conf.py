59f17abbaec0f8d347adfe5f1e87fa69d1fd888b
nanaal <599170260@qq.com>
2015-12-16 00:58:42 +0800

    Create conf.py

---
+++ conf.py
@@ -0,0 +1,12 @@
+#this is the database info configuration file
+#please follow below format
+
+
+# -*- coding: utf-8 -*-
+
+HOST = '127.0.0.1'
+PORT = 1111
+USER = 'root'
+PASSWORD = 'password'
+DBNAME = 'database'
+CHARSET = 'utf8'
